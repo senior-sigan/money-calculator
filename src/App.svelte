5f1dc5d73c3280cb73b76d8c9245f810de01ec6a
Ilya Siganov <ilya.blan4@gmail.com>
2021-11-01 14:00:10 +0600

    extract code into InputScreen from App

--- src/App.svelte
+++ src/App.svelte
@@ -1,76 +1,15 @@
 <script>
-	import Keypad from "./Keypad.svelte";
-	import Numpad from "./Numpad.svelte";
-
-	let inputs = [0];
-	let Component = Numpad;
-	let properties = {};
-	let category = "any";
-	let account = "any";
-
-	function onValueChanged(inputs_) {
-		inputs = inputs_;
-	}
-	function onCategorySelected(categoryName) {
-		category = categoryName;
-		toNumpadState();
-	}
-	function onAccountSelected(accountName) {
-		account = accountName;
-		toNumpadState();
-	}
-	function onSubmit(value) {
-		console.log(value, category, account);
-		inputs = [0];
-		toNumpadState();
-	}
-	function toCateogryState() {
-		Component = Keypad;
-		properties = {
-			onSelected: onCategorySelected,
-			buttons: [],
-		};
-	}
-	function toAccountState() {
-		Component = Keypad;
-		properties = {
-			onSelected: onAccountSelected,
-			buttons: [],
-		};
-	}
-	function toNumpadState() {
-		Component = Numpad;
-		properties = {
-			inputs: inputs,
-			onValueChanged: onValueChanged,
-			onCategory: toCateogryState,
-			onAccount: toAccountState,
-			onSubmit: onSubmit,
-		};
-	}
-
-	toNumpadState();
+    import InputScreen from "./InputScreen.svelte";
 </script>
 
 <main>
-	<div class="display">{inputs.join("")}</div>
-	<svelte:component this={Component} {...properties} />
+    <InputScreen />
 </main>
 
 <style>
-	main {
-		background-color: black;
-		width: 290px;
-		padding: 8px;
-	}
-	.display {
-		padding: 8px;
-		height: 64px;
-		color: white;
-		font-weight: 400;
-		font-size: 52px;
-		align-items: flex-end;
-		display: flex;
-		justify-content: end;
-	}
+    main {
+        background-color: black;
+        width: 290px;
+        padding: 8px;
+    }
 </style>
